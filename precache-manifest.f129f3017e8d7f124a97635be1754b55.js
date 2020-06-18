3f5971234bafe78c028d0cbdfc1e10389b9eff48
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-18 13:48:56 +0430

    first publish

---
+++ precache-manifest.f129f3017e8d7f124a97635be1754b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562fa271cf424dbcda45dad5be6e3e63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6090cd3a9329d68cf732",
+    "url": "/static/css/main.32bacb4b.chunk.css"
+  },
+  {
+    "revision": "8e60e13af2fa4d364795",
+    "url": "/static/js/2.5ca930f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5ca930f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6090cd3a9329d68cf732",
+    "url": "/static/js/main.81ea064a.chunk.js"
+  },
+  {
+    "revision": "33fc7c215b70639d3b88",
+    "url": "/static/js/runtime-main.7f0912e0.js"
+  }
+]);
